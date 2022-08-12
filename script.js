d4d236ed7a62823c226e188a26e60aa9f0f5a32a
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-12 19:24:15 -0300

    resolução requisito 20

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ window.onload = function () {
   const btnEntry = document.querySelector('.entry');
   btnEntry.addEventListener('click', fillForm);
 };
+
+const theText = document.querySelector('#textarea');
+const counterCha = document.querySelector('#counter');
+
+function countingCharacters() {
+  const count = theText.value.length;
+  counterCha.innerHTML = 500 - count;
+}
+
+theText.addEventListener('keyup', countingCharacters);
