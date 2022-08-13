b5ed7ffc0b752527ffe07e1c74c8667a6ff2af19
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-13 15:01:50 -0300

    Requisito 21 em andamento

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const btnEntry = document.querySelector('.entry');
 const btnSend = document.getElementById('submit-btn');
 const theText = document.querySelector('#textarea');
 const counterCha = document.querySelector('#counter');
+const savedName = document.querySelector('#input-name');
+const savedLastName = document.querySelector('#input-lastname');
+const savedEmail = document.querySelector('#input-email');
+const savedHouse = document.querySelector('#house');
+const savedFamily = document.querySelector('#botoes');
+const savedSubjects = document.querySelector('#q2');
+const savedEvaluation = document.querySelector('#avalia');
+const savedComment = document.querySelector('.textarea');
+const mainForm = document.querySelector('#evaluation-form');
+const secondForm = document.querySelector('#form-data');
 
 // eslint-disable-next-line func-names
 window.onload = function () {
@@ -33,3 +43,55 @@ function countingCharacters() {
 }
 
 theText.addEventListener('keyup', countingCharacters);
+
+function choosingFamily() {
+  const choosed = savedFamily.children;
+  for (let index = 0; index < choosed.length; index += 1) {
+    if (choosed[index].checked) {
+      return choosed[index].value;
+    }
+  }
+}
+
+function choosingSubjects() {
+  const allSubjects = savedSubjects.children;
+  const selectedsSubjects = [];
+  for (let index = 0; index < allSubjects.length; index += 1) {
+    if (allSubjects[index].checked) {
+      selectedsSubjects.push(`${allSubjects[index].value}`);
+    }
+  }
+  const subjects = `${selectedsSubjects}`;
+  return subjects;
+}
+
+function choosingEvaluation() {
+  const finalEvaluation = savedEvaluation.children;
+  for (let index = 0; index < finalEvaluation.length; index += 1) {
+    if (finalEvaluation[index].checked) {
+      return finalEvaluation[index].value;
+    }
+  }
+}
+
+function swapDisplay(fill) {
+  fill.preventDefault();
+  const name = document.querySelector('#savedName');
+  name.innerText = `Nome: ${savedName.value} ${savedLastName.value}`;
+  const inputedEmail = document.querySelector('#savedEmail');
+  inputedEmail.innerText = `Email: ${savedEmail.value}`;
+  const choosedHouse = document.querySelector('#choosedHouse');
+  choosedHouse.innerText = `Casa: ${savedHouse.value}`;
+  const choosedFamily = document.querySelector('#choosedFamily');
+  choosedFamily.innerText = `Família: ${choosingFamily()}`;
+  const theSubjects = document.querySelector('#selectedSubjects');
+  theSubjects.innerText = `Matérias: ${choosingSubjects()}`;
+  const choosedEvaluation = document.querySelector('#evaluation');
+  choosedEvaluation.innerText = `Avaliação: ${choosingEvaluation()}`;
+  const yourComment = document.querySelector('#comments');
+  yourComment.innerText = `Observações: ${savedComment.value}`;
+  mainForm.classList.toggle('dontShow');
+  secondForm.classList.toggle('show');
+}
+
+btnSend.addEventListener('click', swapDisplay);
